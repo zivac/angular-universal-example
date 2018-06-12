48c19afea6d2ecd4cd2ea27b024e8d00e4864985
Darko Zivkovic <darko.zivkovic2@gmail.com>
2018-06-12 10:24:42 +0200

    meta tags

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,13 @@ import {Component, OnInit} from '@angular/core';
 export class HomeComponent implements OnInit {
   public message: string;
 
-  constructor() {}
+  constructor(private _title: Title, private _meta: Meta) {
+    this._title.setTitle('home title');
+    this._meta.addTag({
+      content: 'home content',
+      name: 'description'
+    })
+  }
 
   ngOnInit() {
     this.message = 'Hello';
--- src/app/lazy/lazy.module.ts
+++ src/app/lazy/lazy.module.ts
@@ -1,11 +1,22 @@
 import {NgModule, Component} from '@angular/core';
 import {RouterModule} from '@angular/router';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-lazy-view',
   template: `<h3>i'm lazy</h3>`
 })
-export class LazyComponent {}
+export class LazyComponent {
+
+  constructor(private _title: Title, private _meta: Meta) {
+    this._title.setTitle('test title');
+    this._meta.addTag({
+      content: 'test content',
+      name: 'description'
+    })
+  }
+
+}
 
 @NgModule({
   declarations: [LazyComponent],
